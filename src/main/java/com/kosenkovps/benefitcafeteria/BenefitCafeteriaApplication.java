c9c82baec73a4219b205c05d761d4863496b0775
kosenkovps <kosenkovps@ntk.novotelecom.ru>
2023-01-26 22:43:22 +0700

    BC-0:ADD:create a skeleton

---
+++ src/main/java/com/kosenkovps/benefitcafeteria/BenefitCafeteriaApplication.java
@@ -0,0 +1,13 @@
+package com.kosenkovps.benefitcafeteria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BenefitCafeteriaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BenefitCafeteriaApplication.class, args);
+    }
+
+}
